a85309cd1f63fc3c1ef9ab2e07466e7a6f48adc6
benohhh <b3njaminoh@gmail.com>
2018-10-19 20:23:42 -0700

    got html routes working

--- server.js
+++ server.js
@@ -2,3 +2,6 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
+require('./app/routing/htmlRoutes.js')(app)
+
+app.listen(3005, () => {console.log('http://localhost:3005')})
