d407b95c1885a6bdc1145eb83a1035a4646a399c
benohhh <b3njaminoh@gmail.com>
2018-10-21 20:49:53 -0700

    got jumbotron working

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
-const path = require('path')
+const bodyparser = require('body-parser')
+
+app.use(bodyparser.urlencoded({extended: true}))
+app.use(bodyparser.json())
 
 require('./app/routing/htmlRoutes.js')(app)
+
 
 app.listen(3005, () => {console.log('http://localhost:3005')})
