10cc07565282a81e6542f8b215159ccd92777d8b
benohhh <b3njaminoh@gmail.com>
2018-10-22 20:00:35 -0700

    added dummy friend data

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,18 +1,50 @@
 let friends = [
   {
-    name: '',
-    photo: '',
+    name: 'Mario',
+    photo: 'https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fblogs-images.forbes.com%2Folliebarder%2Ffiles%2F2018%2F03%2Fsuper_mario_plumber_new-1200x675.jpg',
     score: [
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
-      '',
+      '4',
+      '5',
+      '2',
+      '4',
+      '3',
+      '2',
+      '1',
+      '3',
+      '2',
+      '4',
+    ]
+  },
+  {
+    name: 'Yoshi',
+    photo: 'https://vignette.wikia.nocookie.net/yoshi/images/8/8d/Yoshi_SSBU.png/revision/latest?cb=20180628045711',
+    score: [
+      '2',
+      '3',
+      '4',
+      '5',
+      '1',
+      '1',
+      '1',
+      '2',
+      '2',
+      '4'a,
+    ]
+  },
+  {
+    name: 'Princess Peach',
+    photo: 'https://vignette.wikia.nocookie.net/nintendo/images/8/83/Peach_MP100.png/revision/latest?cb=20170918143319&path-prefix=en',
+    score: [
+      '1',
+      '1',
+      '5',
+      '5',
+      '5',
+      '1',
+      '2',
+      '2',
+      '3',
+      '4',
     ]
   }
 ]
