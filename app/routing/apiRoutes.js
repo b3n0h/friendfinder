ea40547c613199ca0e7b894bc14ae8b390f68b69
benohhh <b3njaminoh@gmail.com>
2018-10-22 20:31:50 -0700

    finished api routes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,12 +7,31 @@ module.exports = app => {
   })
 
   app.post('/api/friends', (req, res) => {
+
     let soulMate = {
       name: '',
       photo: '',
       scoreDiff: 1000
     }
 
+    let userProfile = req.body
+    let userScore = userProfile.score
+    let totalDiff = 0
 
+    for (let i = 0; i < friends.length; i++) {
+      totalDiff = 0
+
+      for (let o = 0; 0 < friends[i].score.length; o++) {
+        totalDiff += Math.abs(parseInt(userScore[o]) - parseInt(friends[i].score[o]))
+
+        if (totalDiff <= soulMate.scoreDiff) {
+          soulMate.name = friends[i].name
+          soulMate.photo = friends[i].photo
+          soulMate.scoreDiff = totalDiff
+        }
+      }
+    }
+    friends.push(userProfile)
+    res.json(soulMate)
   })
 }
